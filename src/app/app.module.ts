c32246645abe7cd9e5109765931250d418c6bd9f
Fabrício Taveira <fabriciotaveira176@gmail.com>
2024-01-09 15:57:25 -0300

    Creating the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TitleComponent } from './title/title.component';
+import { BackgroundImageComponent } from './background-image/background-image.component';
+import { CardComponent } from './background-image/card/card.component';
+import { TitleMainComponent } from './navbar/title-main/title-main.component';
+import { ButtonComponent } from './navbar/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TitleComponent,
+    BackgroundImageComponent,
+    CardComponent,
+    TitleMainComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navbar/title-main/title-main.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-main',
+  templateUrl: './title-main.component.html',
+  styleUrls: ['./title-main.component.css']
+})
+export class TitleMainComponent implements OnInit {
+
+  logoName:string = "AutoMates";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
