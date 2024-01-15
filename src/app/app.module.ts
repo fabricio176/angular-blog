f461fcce2cb60c544bde76b10160c4356fdd0723
Fabricio Taveira Carvalho Marques <tr26330es@trf1.gov.br>
2024-01-15 10:00:53 -0300

    reformulating the folders path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { TitleComponent } from './title/title.component';
-import { BackgroundImageComponent } from './background-image/background-image.component';
-import { CardComponent } from './background-image/card/card.component';
-import { TitleMainComponent } from './navbar/title-main/title-main.component';
-import { ButtonComponent } from './navbar/button/button.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TitleComponent } from './components/title/title.component';
+import { BackgroundImageComponent } from './components/background-image/background-image.component';
+import { TitleMainComponent } from './components/navbar/title-main/title-main.component';
+import { CardComponent } from './components/background-image/card/card.component';
+import { ButtonComponent } from './components/navbar/button/button.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ButtonComponent } from './navbar/button/button.component';
     BackgroundImageComponent,
     CardComponent,
     TitleMainComponent,
-    ButtonComponent
+    ButtonComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
